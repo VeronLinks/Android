af449bf0f42683690e6d4942e869481ed9faa3b1
veronlinks <verontavasco@gmail.com>
2021-12-29 10:27:18 +0100

    Splash screen done

--- Entrega/FinalProject/app/src/main/java/es/usj/mastertsa/jchueca/finalproject/SplashScreenActivity.kt
+++ Entrega/FinalProject/app/src/main/java/es/usj/mastertsa/jchueca/finalproject/SplashScreenActivity.kt
@@ -1,11 +1,35 @@
 package es.usj.mastertsa.jchueca.finalproject
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import java.util.*
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+
+    companion object {
+        private val SPLASH_SCREEN_DELAY: Long = 2000L
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar!!.hide()
         setContentView(R.layout.activity_splash_screen)
+        val task = object : TimerTask() {
+            override fun run() {
+                val intent = Intent(this@SplashScreenActivity,
+                    MainActivity::class.java
+                )
+                startActivity(intent)
+                finish()
+            }
+        }
+        val timer = Timer()
+        timer.schedule(task, SPLASH_SCREEN_DELAY)
     }
 }
